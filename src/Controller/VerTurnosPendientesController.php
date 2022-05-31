43131ed3117474a466fdc9de516910c690694e87
Jorge <jorgeluiscrespo@gmail.com>
2022-05-31 17:53:25 -0300

    consulta de turnos pendientes de un paciente

---
+++ src/Controller/VerTurnosPendientesController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Pacientes;
+use App\Entity\Turnos;
+use App\Entity\Vacunas;
+use App\Entity\Vacunatorios;
+use App\Service\CustomService;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VerTurnosPendientesController extends AbstractController
+{
+    #[Route('/ver/turnos/pendientes', name: 'app_ver_turnos_pendientes')]
+    public function index(
+        CustomService $cs,
+        ManagerRegistry $doctrine, 
+
+    ): Response
+    {
+        
+        $em = $doctrine->getManager();
+        $mailPaciente = $cs->getUser()['user'];
+        $paciente = $em->getRepository(Pacientes::class)->findOneByEmail($mailPaciente);
+
+        $turnosDelPaciente = $em->getRepository(Turnos::class)->findTurnosByUser($paciente->getId());
+        $turnos = [];
+        foreach($turnosDelPaciente as $turno){
+            if ($turno->getEstado() == 'ASIGNADO'){
+                $turnoStr['vacuna'] = $em->getRepository(Vacunas::class)->findOneById($turno->getVacunaId()->getId())->getNombre();
+                $turnoStr['fecha'] = date_format($turno->getFecha(), "d-m-Y") ;
+                $turnoStr['vacunatorio'] = $em->getRepository(Vacunatorios::class)->findOneById($turno->getVacunatorioId()->getId())->getNombre();
+                // dd($turno, $turnoStr);
+                array_push($turnos, $turnoStr);
+            }
+        }
+        // dd($turnos);
+
+        return $this->render('ver_turnos_pendientes/index.html.twig', [
+            'controller_name' => 'VerTurnosPendientesController',
+            'nombre_paciente' => $paciente->getApellido() . ', ' . $paciente->getNombre(),
+            'turnos' => $turnos,
+        ]);
+    }
+}
