28b0a5468aaf8830740cc507e7317d8853d1d473
Jorge <jorgeluiscrespo@gmail.com>
2022-06-27 20:15:05 -0300

    consulta de notificaciones no leidas

--- src/Controller/NotificacionesPendientesPacienteController.php
+++ src/Controller/NotificacionesPendientesPacienteController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Notificaciones;
 use App\Entity\Pacientes;
 use App\Entity\Turnos;
+use App\Entity\Vacunas;
+use App\Entity\Vacunatorios;
 use App\Service\CustomService;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -31,14 +33,35 @@ class NotificacionesPendientesPacienteController extends AbstractController
             foreach($turnos as $turno){
 
                $notificaciones =  $em->getRepository(Notificaciones::class)->findNotificacionesByTurnoId($turno->getId());
+               $vacuna = $em->getRepository(Vacunas::class)->findOneById($turno->getVacunaId());
+               $vacunatorio = $em->getRepository(Vacunatorios::class)->findOneById($turno->getVacunatorioId());
+
                if (count($notificaciones) > 0 ){
                 foreach($notificaciones as $notificacion){
                     
                     if ( $notificacion->getLeida() == 0){
-                        array_push($notificacionesSinLeer, $notificacion);
                         
+                        $notifArray = array();
+                        $diasAntelacino  = $notificacion->getAntelacion();
+
+                        if (  $diasAntelacino > 0){
+
+                            $fechaTurno = $turno->getFecha();
+                            $fechaNotificacionStr = date("d-m-Y",strtotime(date_format($fechaTurno, "d-m-Y") ."- $diasAntelacino days"));
+                            // dd($fechaNotificacionStr);
+                            
+                            $notifArray['fecha_notificacion'] = $fechaNotificacionStr;
+                        } else {
+                            $notifArray['fecha_notificacion'] ='x';
+                        }
+                        $notifArray['vacuna']  = $vacuna->getNombre();
+                        $notifArray['vacunatorio']  = $vacunatorio->getNombre();
+                        $notifArray['fecha_turno']  = date_format($turno->getFecha(), "d-m-Y");
 
 
+                        
+                        
+                        array_push($notificacionesSinLeer, $notifArray);
 
                     }
 
@@ -54,12 +77,13 @@ class NotificacionesPendientesPacienteController extends AbstractController
             }
         }
 
-        // dd($notificacionesSinLeer, $idNotificacion);
+        //  dd($notificacionesSinLeer, $idNotificacion);
 
 
 
         return $this->render('notificaciones_pendientes_paciente/index.html.twig', [
             'controller_name' => 'NotificacionesPendientesPacienteController',
+            'notificaciones' => $notificacionesSinLeer
         ]);
     }
 }
